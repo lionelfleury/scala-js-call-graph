8b311403e955f5931db43924f0642e3d7af5f503
Lionel Fleury <lionel.fleury@epfl.ch>
2016-03-24 11:47:28 +0100

    add LinkerBackEnd to get SymbolRequirements

--- sbt-scalajs-callgraph/src/main/scala/ch/epfl/sbtplugin/CallGraphPlugin.scala
+++ sbt-scalajs-callgraph/src/main/scala/ch/epfl/sbtplugin/CallGraphPlugin.scala
@@ -1,30 +1,40 @@
 package ch.epfl.sbtplugin
 
-import org.scalajs.core.tools.linker.analyzer.{Analyzer, SymbolRequirement}
+import org.scalajs.core.tools.linker.analyzer.Analyzer
+import org.scalajs.core.tools.linker.backend.{BasicLinkerBackend, LinkerBackend, OutputMode}
 import org.scalajs.sbtplugin.ScalaJSPlugin
-import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import sbt._
 
-import scala.language.implicitConversions
-
 object CallGraphPlugin extends AutoPlugin {
+
   override def trigger = allRequirements
+
   override def requires = ScalaJSPlugin
 
   object autoImport {
-    lazy val callgraph = TaskKey[Unit]("callgraph", "Export call graph to json")
+    lazy val callgraph = TaskKey[Unit]("callgraph", "Export callgraph to json")
   }
 
+  import ScalaJSPlugin.autoImport._
   import autoImport._
 
   override lazy val projectSettings: Seq[Setting[_]] = {
     Seq(
       callgraph := {
+        val semantics = scalaJSSemantics.value
+        val config = LinkerBackend.Config()
+        val linkerBackEnd =
+          new BasicLinkerBackend(semantics, OutputMode.Default, true, config)
+        val symbolRequirement = linkerBackEnd.symbolRequirements
+        val allowAddingSyntheticMethods = false
         val infos = (scalaJSIR in Compile).value.data map (_.info)
         val mapInfos = Analyzer.computeReachability(
-          scalaJSSemantics.value,
-          SymbolRequirement.factory("test").none(),
-          infos, false).classInfos.toMap
+          semantics,
+          symbolRequirement,
+          infos,
+          allowAddingSyntheticMethods
+        ).classInfos.toMap
+
         new Graph(mapInfos)
       }
     )
