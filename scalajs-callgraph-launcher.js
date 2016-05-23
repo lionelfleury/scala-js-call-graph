544e433d68427f2b5d25e0d1e8d2464271935a25
Lionel Fleury <lionel.fleury@epfl.ch>
2016-05-23 12:04:18 +0200

    Add files via upload

--- scalajs-callgraph-launcher.js
+++ scalajs-callgraph-launcher.js
@@ -1,2 +1,2 @@
 ((typeof global === "object" && global &&
-         global["Object"] === Object) ? global : this)["ch"]["epfl"]["callgraph"]["visualization"]["Visualization"]().main();
+         global["Object"] === Object) ? global : this)["ch"]["epfl"]["callgraph"]["visualization"]["view"]["HtmlView"]().main();
